635a4706ac977394fd2522f798c12f7d362af593
Danny Andrews <dannya@spiceworks.com>
2015-01-13 10:33:52 -0600

    Implement minLength and maxLength
    Refs #6

--- demo/demo.js
+++ demo/demo.js
@@ -72,7 +72,7 @@ app.controller('MainCtrl', function ($scope) {
     },
 
     {
-      title: 'Format, Minimum and Maximum',
+      title: 'Format, Minimum, Maximum, MinLength, and MaxLength',
       schema: {
         properties: {
           account_number: {
@@ -80,6 +80,11 @@ app.controller('MainCtrl', function ($scope) {
             format: 'int64',
             minimum: 400000,
             maximum: 900000
+          },
+          bank_name: {
+            type: 'string',
+            minLength: 5,
+            maxLength: 100
           }
         },
         required: ['account_number']
